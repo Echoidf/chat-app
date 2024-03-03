8211d9c6f3e356cc1eccfeca3cac42ebcd2c9322
zql <1241236275@qq.com>
2024-03-03 19:49:21 +0800

    deployed

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-import {app, server} from "./socket/socket.js"
+import { app, server } from "./socket/socket.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -19,6 +22,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/user", userRoutes);
+
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, path.join(__dirname, "frontend", "dist", "index.html")));
+})
 
 server.listen(PORT, () => {
   connectToMongoDB();
